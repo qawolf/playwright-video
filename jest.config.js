2ca043ca006172b615b8c7969b9bfb4d46cf2e1d
Jon Perl <perl.jonathan@gmail.com>
2020-02-28 10:51:38 -0700

    make project structure consistent with playwright-html-selector
    - fix node engine #2
    - move __tests__ -> test

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,10 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node",
-  ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/tests/.*.(test|spec)).(jsx?|tsx?)$',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
 };
