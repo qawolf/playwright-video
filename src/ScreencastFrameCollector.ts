5c39b072b74d6b15858359a16c7af9e3e08bf0b7
Jon Perl <perl.jonathan@gmail.com>
2020-03-11 08:57:44 -0600

    catch Page.screencastFrameAck error

--- src/ScreencastFrameCollector.ts
+++ src/ScreencastFrameCollector.ts
@@ -34,17 +34,12 @@ export class ScreencastFrameCollector extends EventEmitter {
   }
 
   private _listenForFrames(): void {
-    this._client.on('Page.screencastFrame', payload => {
+    this._client.on('Page.screencastFrame', async payload => {
       debug(`received frame with timestamp ${payload.metadata.timestamp}`);
 
-      try {
-        this._client.send('Page.screencastFrameAck', {
-          sessionId: payload.sessionId,
-        });
-      } catch (e) {
-        // nothing we can do
-        debug('error sending screencastFrameAck %j', e.message);
-      }
+      const ackPromise = this._client.send('Page.screencastFrameAck', {
+        sessionId: payload.sessionId,
+      });
 
       if (!payload.metadata.timestamp) {
         debug('skip frame without timestamp');
@@ -56,6 +51,14 @@ export class ScreencastFrameCollector extends EventEmitter {
         received: Date.now(),
         timestamp: payload.metadata.timestamp,
       });
+
+      try {
+        // capture error so it does not propagate to the use
+        await ackPromise;
+      } catch (e) {
+        // nothing we can do
+        debug('error sending screencastFrameAck %j', e.message);
+      }
     });
   }
 
