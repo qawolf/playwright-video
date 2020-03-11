334a683545ddcef61fe5969491dcf0ab29310e6c
Jon Perl <perl.jonathan@gmail.com>
2020-03-11 08:44:45 -0600

    do not sleep, this can leave an open handle for a test
    the user will have to be responsible for sleeping before ending their test
    
    - fix playwright dev dependency version

--- src/ScreencastFrameCollector.ts
+++ src/ScreencastFrameCollector.ts
@@ -4,7 +4,7 @@ import { ChromiumBrowserContext, Page } from 'playwright-core';
 import { CRSession } from 'playwright-core/lib/chromium/crConnection';
 import { ensurePageType } from './utils';
 
-const debug = Debug('playwright-video:FrameCollector');
+const debug = Debug('playwright-video:ScreencastFrameCollector');
 
 export class ScreencastFrameCollector extends EventEmitter {
   public static async create(page: Page): Promise<ScreencastFrameCollector> {
@@ -37,9 +37,14 @@ export class ScreencastFrameCollector extends EventEmitter {
     this._client.on('Page.screencastFrame', payload => {
       debug(`received frame with timestamp ${payload.metadata.timestamp}`);
 
-      this._client.send('Page.screencastFrameAck', {
-        sessionId: payload.sessionId,
-      });
+      try {
+        this._client.send('Page.screencastFrameAck', {
+          sessionId: payload.sessionId,
+        });
+      } catch (e) {
+        // nothing we can do
+        debug('error sending screencastFrameAck %j', e.message);
+      }
 
       if (!payload.metadata.timestamp) {
         debug('skip frame without timestamp');
@@ -65,15 +70,16 @@ export class ScreencastFrameCollector extends EventEmitter {
   public async stop(): Promise<void> {
     if (this._stopped) return;
 
-    debug('stop');
+    debug('stopping');
+
     this._stopped = true;
-    // Screencast API takes time to send frames
-    // Wait 1s for frames to arrive
-    // TODO figure out a better pattern for this
-    await new Promise(resolve => setTimeout(resolve, 1000));
 
     if (this._client._connection) {
+      debug('detaching client');
       await this._client.detach();
+      debug('client detached');
     }
+
+    debug('stopped');
   }
 }
