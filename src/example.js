8d03e1e9dcb515b79a32b955550d72ab2468e4cd
Jon Perl <perl.jonathan@gmail.com>
2020-05-06 09:29:03 -0600

    update to playwright 1.0

--- src/example.js
+++ src/example.js
@@ -3,17 +3,12 @@ const { saveVideo } = require('playwright-video');
 
 (async () => {
   const browser = await chromium.launch();
-
   const context = await browser.newContext();
-
   const page = await context.newPage();
 
   const capture = await saveVideo(page, '/tmp/video.mp4');
-
   await page.goto('http://example.org');
-
   await page.click('a');
-
   await capture.stop();
 
   await browser.close();
