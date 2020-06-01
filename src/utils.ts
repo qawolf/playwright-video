55ddec0c49ef3fc5d2c91bfc6893afb8acf42e70
Steve Davis <steve@celery.club>
2020-06-01 17:42:44 -0400

    Recommend @ffmpeg-installer/ffmpeg instead of ffmpeg-static

--- src/utils.ts
+++ src/utils.ts
@@ -3,8 +3,10 @@ import { ChromiumBrowserContext, Page } from 'playwright-core';
 
 export const getFfmpegFromModule = (): string | null => {
   try {
-    const ffmpegPath = require('ffmpeg-static'); // eslint-disable-line @typescript-eslint/no-var-requires
-    if (ffmpegPath) return ffmpegPath;
+    const ffmpeg = require('@ffmpeg-installer/ffmpeg'); // eslint-disable-line @typescript-eslint/no-var-requires
+    if (ffmpeg.path) {
+      return ffmpeg.path;
+    }
   } catch (e) {} // eslint-disable-line no-empty
 
   return null;
@@ -22,7 +24,7 @@ export const ensureFfmpegPath = (): void => {
   const ffmpegPath = getFfmpegPath();
   if (!ffmpegPath) {
     throw new Error(
-      'pw-video: FFmpeg path not set. Set the FFMPEG_PATH env variable or install ffmpeg-static as a dependency.',
+      'pw-video: FFmpeg path not set. Set the FFMPEG_PATH env variable or install @ffmpeg-installer/ffmpeg as a dependency.',
     );
   }
 
